fb6dc6ba8aedeefce45a4e63c5fce0315997fc73
Draper-dev <tdanieldraper@gmail.com>
2021-08-09 13:37:04 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Layout from './components/Layout'
 function App() {
 
   return (
-    <Router basename="/draper.dev">
+    <BrowserRouter basename="/draper.dev">
       <Layout />
-    </Router>
+    </BrowserRouter>
   )
 }
 
