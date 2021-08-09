0f826aca7108e77f6852e641dbc944de16808fb0
Draper-dev <tdanieldraper@gmail.com>
2021-08-09 13:24:34 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react'
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Layout from './components/Layout'
 function App() {
 
   return (
-    <Router>
+    <Router basename="/draper.dev">
       <Layout />
     </Router>
   )
